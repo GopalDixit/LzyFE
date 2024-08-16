42bbfff42a8702cc53a56b04af4534cca5642fad
Gopal Dixit <98686075+GopalDixit@users.noreply.github.com>
2024-08-17 00:37:28 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,29 @@
-// Footer.jsx
-
 const Footer = () => {
   return (
-    <footer className="w-screen text-black absolute left-0">
-      <div className="">
+    <footer className="w-screen text-black absolute left-0 p-4">
+      <div>
         {/* India Section */}
-        <div className="text-xl flex mt-2 mb-6 ml-4" style={{ fontSize: '1.55rem' }}>
+        <div className="text-xl mt-2 mb-6 ml-4" style={{ fontSize: '1.55rem' }}>
           India
         </div>
-        <hr className="w-screen border-t border-gray-300" />
+        <hr className="w-full border-t border-gray-300 -mt-5" />
 
-        <div className="flex flex-col md: flex-row lg:flex-row justify-between items-center">
+        {/* Footer Options */}
+        <div className="flex flex-col lg:flex-row justify-between items-center lg:items-center">
           {/* Left Side Options */}
-          <div className="flex gap-6 lg:gap-14 lg:mb-0 ml-4">
-            <a href="#" className="hover:underline">About</a>
-            <a href="#" className="text-lg lg:text-2xl hover:text-green-500">Advertising</a>
-            <a href="#" className="text-lg lg:text-2xl hover:text-green-500">Business</a>
-            <a href="#" className="text-lg lg:text-2xl hover:text-green-500">Investor</a>
-            <a href="#" className="text-lg lg:text-2xl hover:text-green-500">Join Our Team</a>
+          <div className="flex flex-row lg:flex-row gap-6 lg:gap-14 lg:mb-0 ml-0 lg:ml-4">
+            <a href="#" className="hover:underline text-xs lg:text-xl">About</a>
+            <a href="#" className="hover:underline text-xs lg:text-2xl">Advertising</a>
+            <a href="#" className="hover:underline text-xs lg:text-2xl">Business</a>
+            <a href="#" className="hover:underline text-xs lg:text-2xl">Investor</a>
+            <a href="#" className="hover:underline text-xs lg:text-2xl">Join Our Team</a>
           </div>
 
           {/* Right Side Options */}
-          <div className="flex gap-6 mr-3">
-            <a href="#" className="hover:underline">Privacy</a>
-            <a href="#" className="hover:underline">Terms</a>
-            <a href="#" className="hover:underline">Help</a>
+          <div className="flex flex-row lg:flex-row gap-6 lg:gap-8 mt-4 lg:mt-0 ml-0 lg:ml-0">
+            <a href="#" className="hover:underline text-xs lg:text-xl">Privacy</a>
+            <a href="#" className="hover:underline text-xs lg:text-xl">Terms</a>
+            <a href="#" className="hover:underline text-xs lg:text-xl">Help</a>
           </div>
         </div>
       </div>
